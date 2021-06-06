d6e5ca19533bc7b62fb10afa51b7cc975abae3f3
Tester <TestTestTest1000@Test.com.ru>
2021-06-06 22:36:55 +0300

    add new components

---
+++ components/Navbar/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__left"></div>
+    <div class="navbar__right">
+      <div v-for="route in routes" :key="route.code" class="navbar__item">
+        <nuxt-link :to="localePath(route.code)">
+          {{ $t(`pages.${route.code}.title`) }}
+        </nuxt-link>
+      </div>
+      <select id="" v-model="$i18n.locale" class="test__dropdown" name="">
+        <option
+          v-for="lang in $i18n.locales"
+          :key="lang.code"
+          :value="lang.code"
+        >
+          {{ lang.name }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    routes: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
