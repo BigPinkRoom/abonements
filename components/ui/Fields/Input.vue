b7173b58ef858fa1b87f0dfd698c439c80e55c53
Tester <TestTestTest1000@Test.com.ru>
2021-07-24 16:49:54 +0300

    add 2 ui 2 ui components

---
+++ components/ui/Fields/Input.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="field field_input">
+    <input :ref="id" :value="value" :type="type" :name="name" :placeholder="placeholder" @input="updateValue($event.target.value)" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldInput',
+  props: {
+    id: {
+      type: [String, Number],
+    },
+    name: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: [String, Number],
+    },
+  },
+  methods: {
+    updateValue(value) {},
+  },
+};
+</script>
---
+++ components/ui/Forms/MainForm/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form">
+    <form class="form__content" @submit.prevent="emitSubmit">
+      <fieldset class="form-content">
+        <legend class="form__legend">
+          <slot name="title"></slot>
+        </legend>
+
+        <ul class="form-content__group">
+          <li v-for="field in fields" :key="field.id" class="form-content__item">
+            <label :for="field.name" class="form-content__label">
+              {{ $t(field.title) }}
+            </label>
+            <v-input :id="field.id" :name="field.name" :placeholder="$t(field.placeholder)" :type="field.type" />
+          </li>
+          <v-input type="submit" :value="$t(submitText)" />
+        </ul>
+      </fieldset>
+    </form>
+  </div>
+</template>
+<script>
+import vInput from '../../Fields/Input';
+
+export default {
+  name: 'Form',
+  components: {
+    vInput,
+  },
+  props: {
+    fields: {
+      type: Array,
+      default: () => [],
+    },
+    submitText: {
+      type: String,
+      default: 'forms.login.submit',
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    emitSubmit(event) {
+      this.$emit('formSubmit', event);
+    },
+  },
+};
+</script>
