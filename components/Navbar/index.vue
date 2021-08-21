55ba17911b4912222b52508da31bf39c0278eb6b
Tester <TestTestTest1000@Test.com.ru>
2021-08-21 21:13:52 +0300

    change router logic. And move css to component

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -2,8 +2,12 @@
   <div class="navbar">
     <div class="navbar__left"></div>
     <div class="navbar__right">
-      <div v-for="route in routes" :key="route.code" class="navbar__item">
-        <nuxt-link :to="localePath(route.code)" :event="route.disabled ? '' : 'click'" @click.native="startAction(route.action)">
+      <div v-for="route in checkedRoutes" :key="route.code" class="navbar__item">
+        <nuxt-link
+          :to="localePath(route.code)"
+          :event="route.disabled ? '' : 'click'"
+          @click.native="startAction(route.action)"
+        >
           {{ $t(`pages.${route.code}.title`) }}
         </nuxt-link>
       </div>
@@ -24,6 +28,29 @@ export default {
       default: () => [],
     },
   },
+  computed: {
+    checkedRoutes() {
+      const checkedRoutes = [];
+
+      this.routes.forEach((route) => {
+        let check = false;
+
+        if (this.$auth.loggedIn) {
+          check = route.forRoles.find((role) => role === this.$auth.user.user_role);
+        }
+
+        if (!this.$auth.loggedIn) {
+          check = route.forRoles.find((role) => role === 'guest');
+        }
+
+        if (check) {
+          checkedRoutes.push(route);
+        }
+      });
+
+      return checkedRoutes;
+    },
+  },
   methods: {
     startAction(action) {
       if (action) action();
@@ -31,3 +58,40 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  width: 100%;
+  padding: 0.6rem 1rem;
+
+  background-color: $color-green-dark;
+
+  &__right {
+    display: flex;
+    flex-direction: row;
+    margin-left: auto;
+  }
+
+  &__item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-right: 1.25rem;
+
+    color: $color-green;
+    font-size: 1rem;
+    text-transform: uppercase;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    & a {
+      color: $color-green-light;
+      line-height: 1.35;
+      text-decoration: none;
+    }
+  }
+}
+</style>
