9354bcc4f3c581ab34f6917a846f224ae7c990f6
Tester <TestTestTest1000@Test.com.ru>
2021-08-22 22:24:08 +0300

    add new component (button)

---
+++ components/ui/Buttons/MainButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button :ref="id" @click="clickHandler" class="button" :class="styleType">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    id: {
+      type: [String, Number],
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    styleType: {
+      type: String,
+    },
+  },
+  methods: {
+    clickHandler() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  padding: 0.4rem;
+
+  color: var(--color-tertiary);
+
+  background-color: var(--color-primary-dark);
+  border: 0;
+
+  cursor: pointer;
+}
+</style>
