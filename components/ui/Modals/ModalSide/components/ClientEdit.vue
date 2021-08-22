1dc9cadba506b0361739965e72ef7e22debe71ca
Tester <TestTestTest1000@Test.com.ru>
2021-08-22 22:25:47 +0300

    add new component (ClientEdit)

---
+++ components/ui/Modals/ModalSide/components/ClientEdit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-input type="password"></v-input>
+    <v-input type="password"></v-input>
+    <v-input type="password"></v-input>
+    <v-input type="password"></v-input>
+    <v-input type="password"></v-input>
+  </div>
+</template>
+
+<script>
+import vInput from '@/components/ui/Fields/Input';
+
+export default {
+  name: 'ClientEdit',
+  components: {
+    vInput,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
