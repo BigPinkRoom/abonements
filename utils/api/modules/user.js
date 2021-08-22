8ef84a9b9f9e16d03ce6c9fcf1b3af63754bc838
Tester <TestTestTest1000@Test.com.ru>
2021-08-22 22:31:25 +0300

    add 'login' and change 'logout' logic

--- utils/api/modules/user.js
+++ utils/api/modules/user.js
@@ -1,4 +1,5 @@
 import ApiModule from '../module';
+// import ApiResponse from '../response';
 
 export default class User extends ApiModule {
   constructor(app) {
@@ -11,7 +12,28 @@ export default class User extends ApiModule {
     return await this.request('/auth/signup', 'post', params);
   }
 
+
+  // TODO remove (here need to only api)?
   async logout() {
-    return await this.request('/auth/logout', 'get');
+    // return await this.request('/auth/logout', 'get');
+    await this.context.$auth.logout();
+
+    const answer = 'Succussefully logged out';
+
+    return answer;
+  }
+
+  // TODO remove (here need to only api)?
+  async login(user) {
+    console.log('user login', user);
+
+    await this.context.$auth.loginWith('cookie', {
+      data: user,
+    });
+    const answer = 'User is logged';
+
+    // answer = new ApiResponse(answer);
+
+    return answer;
   }
 }
