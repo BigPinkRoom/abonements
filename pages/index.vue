c71729a08906f3e5f7a617ee03ddb9ffb4ed7d2e
Tester <TestTestTest1000@Test.com.ru>
2021-08-22 22:28:45 +0300

    add several components and logic to main page module

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="container">
-    <table>
-      <tbody></tbody>
-    </table>
+    <modal-side
+      :active="modalSideActive"
+      v-show="modalSideActive"
+      sticky
+      position="left"
+      @close="closeModalSide"
+      title="Client edit"
+    >
+      <template #content><client-edit title="Client edit" /></template>
+    </modal-side>
+    <abonements-table @newClient="openAddNewClient" />
   </div>
 </template>
 
 <script>
+import AbonementsTable from '@/components/AbonementsTable';
+import ModalSide from '@/components/ui/Modals/ModalSide';
+import ClientEdit from '@/components/ui/Modals/ModalSide/components/ClientEdit';
+
 export default {
   name: 'Index',
+  middleware: 'auth-employee-access',
+  components: {
+    AbonementsTable,
+    ModalSide,
+    ClientEdit,
+  },
   data() {
-    return {};
+    return {
+      modalSideActive: false,
+    };
   },
   async fetch() {},
   computed: {},
-  methods: {},
+  methods: {
+    openAddNewClient() {
+      this.modalSideActive = true;
+    },
+    closeModalSide() {
+      this.modalSideActive = false;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -22,19 +49,10 @@ export default {
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: left;
   width: 100%;
   height: calc(100vh - 2.5rem);
 
-  background-color: #ccc;
-}
-
-.title {
-  color: #eee;
-  font-size: 2rem;
-  font-family: Arial, sans-serif;
-
-  text-transform: uppercase;
-  text-decoration: none;
+  background-color: var(--color-test);
 }
 </style>
