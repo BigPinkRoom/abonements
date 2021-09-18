f64fccbe46fb8a54f04fa9da1e08ac25aa2885c8
Tester <TestTestTest1000@Test.com.ru>
2021-09-18 13:57:50 +0300

    add uuid module. And add its module for ContentClientEdit component

--- components/ui/Modals/ModalSide/components/ContentClientEdit/Main/components/MainGroupClient.vue
+++ components/ui/Modals/ModalSide/components/ContentClientEdit/Main/components/MainGroupClient.vue
@@ -2,36 +2,36 @@
   <fieldset class="client-main__fieldset">
     <legend>{{ $t(`forms.client.${actionType}.fieldsets.client.label`) }}</legend>
 
-    <label :for="`clientSurname_${_uid}`">{{
+    <label :for="`clientSurname_${uuid}`">{{
       $t(`forms.client.${actionType}.fieldsets.client.fields.surname.label`)
     }}</label>
     <v-input
-      :id="`clientSurname_${_uid}`"
+      :id="`clientSurname_${uuid}`"
       type="text"
       :placeholder="$t(`forms.client.${actionType}.fieldsets.client.fields.surname.placeholder`)"
     ></v-input>
 
-    <label :for="`clientName_${_uid}`">{{ $t(`forms.client.${actionType}.fieldsets.client.fields.name.label`) }}</label>
+    <label :for="`clientName_${uuid}`">{{ $t(`forms.client.${actionType}.fieldsets.client.fields.name.label`) }}</label>
     <v-input
-      :id="`clientName_${_uid}`"
+      :id="`clientName_${uuid}`"
       type="text"
       :placeholder="$t(`forms.client.${actionType}.fieldsets.client.fields.name.placeholder`)"
     ></v-input>
 
-    <label :for="`clientPatronymic_${_uid}`">{{
+    <label :for="`clientPatronymic_${uuid}`">{{
       $t(`forms.client.${actionType}.fieldsets.client.fields.patronymic.label`)
     }}</label>
     <v-input
-      :id="`clientPatronymic_${_uid}`"
+      :id="`clientPatronymic_${uuid}`"
       type="text"
       :placeholder="$t(`forms.client.${actionType}.fieldsets.client.fields.patronymic.placeholder`)"
     ></v-input>
 
-    <label :for="`clientBirthday_${_uid}`">{{
+    <label :for="`clientBirthday_${uuid}`">{{
       $t(`forms.client.${actionType}.fieldsets.client.fields.birthday.label`)
     }}</label>
     <v-input
-      :id="`clientBirthday_${_uid}`"
+      :id="`clientBirthday_${uuid}`"
       type="text"
       :placeholder="$t(`forms.client.${actionType}.fieldsets.client.fields.birthday.placeholder`)"
     ></v-input>
@@ -40,6 +40,7 @@
 
 <script>
 import vInput from '@/components/ui/Fields/Input';
+import { uuid } from 'vue-uuid';
 
 export default {
   name: 'MainGroupClient',
@@ -52,7 +53,12 @@ export default {
     },
   },
   data() {
-    return {};
+    return {
+      uuid: '',
+    };
+  },
+  mounted() {
+    this.uuid = uuid.v4();
   },
 };
 </script>
