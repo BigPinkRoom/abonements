029ca86691f3e3a1a5eb9d239d66b643048d28e0
Tester <TestTestTest1000@Test.com.ru>
2022-01-05 22:57:36 +0300

    changes in Select component

--- components/ui/Selects/Select.vue
+++ components/ui/Selects/Select.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="select">
-    <select :id="id" :name="name" class="select__field" @input="updateValue($event.target.value)" />
-    <option
-      v-for="option in optionsList"
-      :key="option.id"
-      :disabled="option.disabled"
-      :selected="option.selected"
-      :value="option.value"
-      class="select__option"
-    >
-      {{ option.text }}
-    </option>
+    <select :id="id" :name="name" class="select__field" @input="updateValue($event.target.value)">
+      <option v-if="placeholder" disabled selected>
+        {{ $t(placeholder) }}
+      </option>
+      <option
+        v-for="option in optionsList"
+        :key="option.id"
+        :disabled="option.disabled"
+        :selected="option.selected"
+        :value="option.value"
+        class="select__option"
+      >
+        {{ option.text }}
+      </option>
+    </select>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FieldInput',
+  name: 'Select',
   props: {
     id: {
       type: [String, Number],
     },
     name: {
       type: String,
+    },
+    placeholder: {
+      type: String,
+      default: () => '',
     },
     optionsList: {
       type: Array,
