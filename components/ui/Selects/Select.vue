8d39f1d935c55edcb63ec71751a66b6c83373b9b
Tester <TestTestTest1000@Test.com.ru>
2022-01-05 22:14:38 +0300

    add new Select component

---
+++ components/ui/Selects/Select.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="select">
+    <select :id="id" :name="name" class="select__field" @input="updateValue($event.target.value)" />
+    <option
+      v-for="option in optionsList"
+      :key="option.id"
+      :disabled="option.disabled"
+      :selected="option.selected"
+      :value="option.value"
+      class="select__option"
+    >
+      {{ option.text }}
+    </option>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldInput',
+  props: {
+    id: {
+      type: [String, Number],
+    },
+    name: {
+      type: String,
+    },
+    optionsList: {
+      type: Array,
+    },
+  },
+  methods: {
+    updateValue(value) {},
+  },
+};
+</script>
+<style lang="scss" scoped>
+.select {
+  &__field {
+    width: 100%;
+  }
+  &__option {
+  }
+}
+</style>
