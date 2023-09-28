1add3ef406aabf2f421380feee7d235cce418ff0
Name <you>
2023-09-28 20:12:31 +0300

    add new radio-button styles

--- components/ui/RadioButtons/mainRadioButton.vue
+++ components/ui/RadioButtons/mainRadioButton.vue
@@ -70,4 +70,44 @@ export default {
     margin-right: 0;
   }
 }
+
+input[type='radio'] {
+  --s: 1.6rem; /* control the size */
+  --c: var(--color-main); /* the active color */
+
+  height: var(--s);
+  aspect-ratio: 1;
+  border: calc(var(--s) / 8) solid var(--color-main-tertiary-light);
+  padding: calc(var(--s) / 8);
+  background: radial-gradient(farthest-side, var(--c) 100%, #0000) 50%/0 0 no-repeat content-box;
+  border-radius: 50%;
+  outline-offset: calc(var(--s) / 10);
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  cursor: pointer;
+  font-size: inherit;
+  transition: 0.3s;
+}
+input[type='radio']:checked {
+  border-color: var(--c);
+  background-size: 100% 100%;
+}
+
+input[type='radio']:disabled {
+  background: linear-gradient(#939393 0 0) 50%/100% 20% no-repeat content-box;
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+label {
+  display: flex;
+  align-items: center;
+  margin: 5px 0;
+  cursor: pointer;
+}
+
+label input {
+  margin-right: 5px;
+}
 </style>
